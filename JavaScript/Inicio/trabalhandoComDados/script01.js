d86f4702d7e60b86c56b5dc94b6097cf41972421
Cleyton Souza <cleyton.souza@gmail.com>
2023-11-16 21:52:53 -0300

    Criado arquivos script01.js

---
+++ JavaScript/Inicio/trabalhandoComDados/script01.js
@@ -0,0 +1,26 @@
+// camelCase
+var fabricanteCarro = "Renault"
+let modeloCarro = 'Logan'
+var precoCarro = 16000
+var anoCarro = 2018
+
+console.log("Carros")
+console.log(fabricanteCarro)
+console.log(modeloCarro)
+console.log(precoCarro)
+console.log(anoCarro)
+
+console.log("Fabricante: " + fabricanteCarro + ", modelo: " + modeloCarro + ", preço: " + precoCarro + ", ano: " + anoCarro + ".") 
+
+// modeloCarro = 'sandero'
+// console.log(modeloCarro)
+
+
+
+// transformar texto em comentário 
+// "selecionar" 
+// 'CTRL + K' e "CTRL + C"
+// desfazer 'CTRL + K' e 'CTRL + U'
+
+
+Antes instalar o NODE.JS e 
